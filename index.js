6c944c49e83b581207f726ea318988dcb3d9f40f
Hanna <hanna-marii.kriisa@voco.ee>
2022-12-19 13:58:19 +0200

    Add static files, css and js for styling

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
 
-app.get('/',(rew,res)=> {
- res.send('Hello world')
+// add template engine
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// static files - CSS, JS, Images
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+ res.render('index')
 })
 
 app.listen(3005)
